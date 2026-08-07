719cf6cb35e6912d55ad4ece501fbeb5e7701046
sumadri <sumadri.sunil@gmail.com>
2026-08-07 15:11:26 +0530

    updated files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://reddydentalfl.com"),
+  metadataBase: new URL("https://reddydentalfl-com-git-nextjs-conquerorstechus-6043s-projects.vercel.app/"),
   title: "Dentist Near Me in St. Petersburg, FL | Reddy Dental",
   description:
     "Reddy Dental is a dentist near me located in St. Petersburg, FL 33707 for all your family and cosmetic dentistry needs.",
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     title: "Dentist Near Me in St. Petersburg, FL | Reddy Dental",
     description:
       "Reddy Dental is a dentist near me located in St. Petersburg, FL 33707 for all your family and cosmetic dentistry needs.",
-    url: "https://reddydentalfl.com/",
+    url: "https://reddydentalfl-com-git-nextjs-conquerorstechus-6043s-projects.vercel.app/",
     images: [
       {
         url: "/og-image.jpg",
